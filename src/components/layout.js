ba729351e006e6418ca2eeb2de51bf57c800fe0b
Monica Williams <jay.m.howard@gmail.com>
2021-05-13 13:05:15 -0500

    updated metadata

--- src/components/layout.js
+++ src/components/layout.js
@@ -18,6 +18,7 @@ const Layout = ({ children }) => {
       site {
         siteMetadata {
           title
+          author
         }
       }
     }
@@ -39,9 +40,9 @@ const Layout = ({ children }) => {
             marginTop: `2rem`,
           }}
         >
-          © {new Date().getFullYear()}, Built with
+          © {new Date().getFullYear()}, Built by
           {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+          <a href="https://www.linkedin.com/in/monica-howard">{data.site.siteMetadata?.author}</a>
         </footer>
       </div>
     </>
